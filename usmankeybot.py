30c5a01a5ef10fb6d44783b3003a6b267f615456
anixunter <105407320+anixunter@users.noreply.github.com>
2023-08-05 15:22:01 +0545

    Update usmankeybot.py
    massive update:
    1) update to reflect changes in web service site
    2) send msg to chat while loading the site as well

--- usmankeybot.py
+++ usmankeybot.py
@@ -1,5 +1,6 @@
 import requests
 import telebot
+from flask import Flask
 
 # Replace YOUR_TELEGRAM_BOT_TOKEN with your actual Telegram Bot Token
 TELEGRAM_BOT_TOKEN = '6372898666:AAFpyXd6TWUz2YtKeuOq8bVaRKZyxVYw55I'
@@ -10,26 +11,37 @@ WEBSITE_URL = 'https://usmanbypass.top/usm83.php'
 # Initialize the bot with your token
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-# Function to scrape the fixed website and return the body content
+# Initialize a variable to store the scraped website's body
+scraped_body = None
+
+# Function to scrape the fixed website and store the body content
 def scrape_website():
+    global scraped_body
     response = requests.get(WEBSITE_URL)
     if response.status_code == 200:
-        return response.text
+        scraped_body = response.text
+        # Send the website body to the Telegram bot
+        bot.send_message(chat_id='-1001693818855', text=scraped_body)
     else:
-        return None
+        scraped_body = None
 
 # Function to handle the usman/key/usmankey command
 @bot.message_handler(func=lambda message: message.text.lower() in ['usman', 'key', 'usmankey'])
 def get_website(message):
-    body = scrape_website()
-    if body:
-        bot.reply_to(message, body)
-    else:
-        bot.reply_to(message, "Failed to fetch the website body.")
+    scrape_website()  # Call the function to scrape the website
+    bot.reply_to(message, scraped_body or "Failed to fetch the website body.")
 
-def main():
-    # Start the Bot using long polling
-    bot.polling()
+# Flask app initialization
+app = Flask(__name__)
 
+# Route to show the scraped body on the web page
+@app.route('/')
+def show_scraped_body():
+    scrape_website()  # Call the function to scrape the website
+    return f"<pre>{scraped_body or 'Failed to fetch the website body.'}</pre>"
 if __name__ == "__main__":
-    main()
+    # Start the Flask app and the Telegram bot concurrently
+    import threading
+    flask_thread = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
+    flask_thread.start()
+    bot.polling()
