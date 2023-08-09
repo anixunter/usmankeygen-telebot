3b6c1a9ab07497f7556bafc293eca37b17dc6dcf
anixunter <105407320+anixunter@users.noreply.github.com>
2023-08-09 13:23:56 +0545

    added dakukey retrive function
    added dakukey generation functions

--- usmankeybot.py
+++ usmankeybot.py
@@ -8,10 +8,12 @@ TELEGRAM_BOT_TOKEN = '6372898666:AAFpyXd6TWUz2YtKeuOq8bVaRKZyxVYw55I'
 # Fixed URL for scraping
 WEBSITE_URL = 'https://usmanbypass.top/usm83.php'
 
+WEBURL2 = 'http://dakukey076.atwebpages.com/redirectkey076.php'
+
 # Initialize the bot with your token
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-# Function to scrape the fixed website and return the body content
+# Function to scrape the usmankey website and return the body content
 def scrape_website():
     response = requests.get(WEBSITE_URL)
     if response.status_code == 200:
@@ -19,10 +21,27 @@ def scrape_website():
     else:
         return None
 
-# Function to handle the usman/key/usmankey command
+# Function to scrape the dakukey website and return the key content
+def scrape_website2():
+    response = requests.get(WEBURL2)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+# Function to handle the usman command
 @bot.message_handler(func=lambda message: message.text.lower() == 'usman')
 def get_website(message):
     body = scrape_website()
+    if body:
+        bot.send_message(message.chat.id, body)
+    else:
+        bot.send_message(message.chat.id, "Failed to fetch the website body.")
+
+# Function to handle the daku command
+@bot.message_handler(func=lambda message: message.text.lower() == 'daku')
+def get_website(message):
+    body = scrape_website2()
     if body:
         bot.send_message(message.chat.id, body)
     else:
