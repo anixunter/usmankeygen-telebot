0d56bc32e822b9db8a9a176ec4c6a0cdd88069fa
anixunter <105407320+anixunter@users.noreply.github.com>
2023-08-09 13:33:13 +0545

    added beautifulsoup
    added beautifulsoup to get the value only for daku key

--- usmankeybot.py
+++ usmankeybot.py
@@ -1,6 +1,7 @@
 import requests
 import telebot
 from flask import Flask, request
+from bs4 import BeautifulSoup
 
 # Replace YOUR_TELEGRAM_BOT_TOKEN with your actual Telegram Bot Token
 TELEGRAM_BOT_TOKEN = '6372898666:AAFpyXd6TWUz2YtKeuOq8bVaRKZyxVYw55I'
@@ -21,12 +22,30 @@ def scrape_website():
     else:
         return None
 
-# Function to scrape the dakukey website and return the key content
+
 def scrape_website2():
-    response = requests.get(WEBURL2)
-    if response.status_code == 200:
-        return response.text
-    else:
+    try:
+        # Send a GET request to the URL
+        response = requests.get(WEBURL2)
+        
+        # Check if the request was successful
+        response.raise_for_status()
+        
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find the input element with the specified attributes
+        input_element = soup.find('input', {'type': 'text', 'id': 'Username'})
+        
+        # Extract the value attribute of the input element
+        if input_element:
+            value = input_element.get('value')
+            return value
+        else:
+            return None
+        
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
         return None
 
 # Function to handle the usman command
@@ -41,11 +60,11 @@ def get_website(message):
 # Function to handle the daku command
 @bot.message_handler(func=lambda message: message.text.lower() == 'daku')
 def get_website(message):
-    body = scrape_website2()
+    value = scrape_website2()
     if body:
-        bot.send_message(message.chat.id, body)
+        bot.send_message(message.chat.id, value)
     else:
-        bot.send_message(message.chat.id, "Failed to fetch the website body.")
+        bot.send_message(message.chat.id, "Failed to fetch the website content.")
 
 # Flask app initialization
 app = Flask(__name__)
