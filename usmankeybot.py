63a9780727cca6d351724e7763fa934fe7e3ae64
anixunter <105407320+anixunter@users.noreply.github.com>
2023-08-11 12:46:12 +0545

    removed dakukey gen command
    removed daku key gen command cuz of server response delay

--- usmankeybot.py
+++ usmankeybot.py
@@ -1,15 +1,12 @@
 import requests
 import telebot
 from flask import Flask, request
-from bs4 import BeautifulSoup
 
 # Replace YOUR_TELEGRAM_BOT_TOKEN with your actual Telegram Bot Token
 TELEGRAM_BOT_TOKEN = '6372898666:AAFpyXd6TWUz2YtKeuOq8bVaRKZyxVYw55I'
 
 # Fixed URL for scraping
 WEBSITE_URL = 'https://usmanbypass.top/usm83.php'
-
-WEBURL2 = 'http://dakukey076.atwebpages.com/redirectkey076.php'
 
 # Initialize the bot with your token
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
@@ -22,32 +19,6 @@ def scrape_website():
     else:
         return None
 
-
-def scrape_website2():
-    try:
-        # Send a GET request to the URL
-        response = requests.get(WEBURL2)
-        
-        # Check if the request was successful
-        response.raise_for_status()
-        
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Find the input element with the specified attributes
-        input_element = soup.find('input', {'type': 'text', 'id': 'Username'})
-        
-        # Extract the value attribute of the input element
-        if input_element:
-            value = input_element.get('value')
-            return value
-        else:
-            return None
-        
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
-
 # Function to handle the usman command
 @bot.message_handler(func=lambda message: message.text.lower() == 'usman')
 def get_website(message):
@@ -56,15 +27,6 @@ def get_website(message):
         bot.send_message(message.chat.id, body)
     else:
         bot.send_message(message.chat.id, "Failed to fetch the website body.")
-
-# Function to handle the daku command
-@bot.message_handler(func=lambda message: message.text.lower() == 'daku')
-def get_website2(message):
-    body = scrape_website2()
-    if body:
-        bot.send_message(message.chat.id, body)
-    else:
-        bot.send_message(message.chat.id, "Failed to fetch the website content.")
 
 # Flask app initialization
 app = Flask(__name__)
